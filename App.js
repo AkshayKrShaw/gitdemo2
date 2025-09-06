29b161c946cb6c313cb7bf0bc677331295e5e398
AkshayKrShaw <akshaykumarshaw11@gmail.com>
2025-09-06 20:43:07 +0530

    adding some node code

--- App.js
+++ App.js
@@ -1,2 +1,7 @@
 console.log("Om Namo Bhagwate Vasudevay Namah");
 console.log("“Om Krishnaya Vasudevaya Haraye Paramatmane, Pranata: Kleshanashaya Govindaya Namo Nama:”");
+console.log(process.argv);
+let FatherName = process.argv[2];
+let MotherName = process.argv[3];
+let BrotherName = process.argv[4];
+console.log(`Father Name: ${FatherName}\nMother Name: ${MotherName}\nBrother Name: ${BrotherName}\n`);
