89f2ea6206d7c7652279bf69962a85fe5e3c7c51
AkshayKrShaw <akshaykumarshaw11@gmail.com>
2025-09-07 18:07:46 +0530

    adding some node code require

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Eadhe Radhe......");
+let math=require("./App.js");
+
+console.log(math.add(2,3));
+console.log(math.add(33,3));
+console.log(math.add(2,3));
+console.log(math.add(3,3));
